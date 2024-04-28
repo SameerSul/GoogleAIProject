590e732999754d743bf7204fc49af51289baffb3
Apep <mailsafe923@gmail.com>
2024-04-28 22:30:23 +0300

    yikes

--- mainAI.py
+++ mainAI.py
@@ -10,7 +10,7 @@ model = genai.GenerativeModel('gemini-1.0-pro-latest')
 name = 'Sameer Suleman'
 response = model.generate_content(f"If my name is {name} return both my first name and lastname backwards")
 print(response.text)
-
+# sam is secretly a jew
 
 # to send data into json what I was thinking of was receiving and assigning a bunch of values into new variables,
 # then throwing that into a json template which we will then put into json/xml file which we can then use to update the website? lol
